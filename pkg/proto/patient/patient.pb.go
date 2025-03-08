a00fd00e8d81618960e8756a0c0ed0ec765fe585
Oliver Otchere <oliverotchere4@gmail.com>
2025-03-08 15:06:41 +0000

    added buffs

--- pkg/proto/patient/patient.pb.go
+++ pkg/proto/patient/patient.pb.go
@@ -170,6 +170,58 @@ func (x *Patient) GetUpdatedAt() *timestamppb.Timestamp {
 	return nil
 }
 
+type UpdatePatientDeviceId struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Id            uint32                 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	DeviceId      string                 `protobuf:"bytes,2,opt,name=deviceId,proto3" json:"deviceId,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *UpdatePatientDeviceId) Reset() {
+	*x = UpdatePatientDeviceId{}
+	mi := &file_pkg_proto_patient_proto_msgTypes[1]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *UpdatePatientDeviceId) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdatePatientDeviceId) ProtoMessage() {}
+
+func (x *UpdatePatientDeviceId) ProtoReflect() protoreflect.Message {
+	mi := &file_pkg_proto_patient_proto_msgTypes[1]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdatePatientDeviceId.ProtoReflect.Descriptor instead.
+func (*UpdatePatientDeviceId) Descriptor() ([]byte, []int) {
+	return file_pkg_proto_patient_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *UpdatePatientDeviceId) GetId() uint32 {
+	if x != nil {
+		return x.Id
+	}
+	return 0
+}
+
+func (x *UpdatePatientDeviceId) GetDeviceId() string {
+	if x != nil {
+		return x.DeviceId
+	}
+	return ""
+}
+
 var File_pkg_proto_patient_proto protoreflect.FileDescriptor
 
 var file_pkg_proto_patient_proto_rawDesc = string([]byte{
@@ -208,8 +260,13 @@ var file_pkg_proto_patient_proto_rawDesc = string([]byte{
 	0x64, 0x5f, 0x61, 0x74, 0x18, 0x0e, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f,
 	0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d,
 	0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x52, 0x09, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x41,
-	0x74, 0x42, 0x13, 0x5a, 0x11, 0x70, 0x6b, 0x67, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x70,
-	0x61, 0x74, 0x69, 0x65, 0x6e, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x74, 0x22, 0x43, 0x0a, 0x15, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x50, 0x61, 0x74, 0x69, 0x65,
+	0x6e, 0x74, 0x44, 0x65, 0x76, 0x69, 0x63, 0x65, 0x49, 0x64, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x02, 0x69, 0x64, 0x12, 0x1a, 0x0a, 0x08, 0x64, 0x65,
+	0x76, 0x69, 0x63, 0x65, 0x49, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x64, 0x65,
+	0x76, 0x69, 0x63, 0x65, 0x49, 0x64, 0x42, 0x13, 0x5a, 0x11, 0x70, 0x6b, 0x67, 0x2f, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x2f, 0x70, 0x61, 0x74, 0x69, 0x65, 0x6e, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 })
 
 var (
@@ -224,15 +281,16 @@ func file_pkg_proto_patient_proto_rawDescGZIP() []byte {
 	return file_pkg_proto_patient_proto_rawDescData
 }
 
-var file_pkg_proto_patient_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_pkg_proto_patient_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_pkg_proto_patient_proto_goTypes = []any{
 	(*Patient)(nil),               // 0: proto.Patient
-	(*timestamppb.Timestamp)(nil), // 1: google.protobuf.Timestamp
+	(*UpdatePatientDeviceId)(nil), // 1: proto.UpdatePatientDeviceId
+	(*timestamppb.Timestamp)(nil), // 2: google.protobuf.Timestamp
 }
 var file_pkg_proto_patient_proto_depIdxs = []int32{
-	1, // 0: proto.Patient.dob:type_name -> google.protobuf.Timestamp
-	1, // 1: proto.Patient.created_at:type_name -> google.protobuf.Timestamp
-	1, // 2: proto.Patient.updated_at:type_name -> google.protobuf.Timestamp
+	2, // 0: proto.Patient.dob:type_name -> google.protobuf.Timestamp
+	2, // 1: proto.Patient.created_at:type_name -> google.protobuf.Timestamp
+	2, // 2: proto.Patient.updated_at:type_name -> google.protobuf.Timestamp
 	3, // [3:3] is the sub-list for method output_type
 	3, // [3:3] is the sub-list for method input_type
 	3, // [3:3] is the sub-list for extension type_name
@@ -251,7 +309,7 @@ func file_pkg_proto_patient_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_pkg_proto_patient_proto_rawDesc), len(file_pkg_proto_patient_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
