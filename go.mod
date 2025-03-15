6eb0aed8fdf9cdf0f0b8c505396bfc5f51640d35
Oliver <oliver.otchere@deltaflare.com>
2025-03-15 01:39:27 +0000

    switch to aws sdk v2

--- go.mod
+++ go.mod
@@ -25,6 +25,21 @@ require (
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/metric v0.48.1 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/internal/resourcemapping v0.48.1 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.36.3 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.10 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.18.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.25.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.29.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.17 // indirect
+	github.com/aws/smithy-go v1.22.2 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cncf/xds/go v0.0.0-20240905190251-b4127c9b8d78 // indirect
 	github.com/envoyproxy/go-control-plane/envoy v1.32.3 // indirect
@@ -77,7 +92,10 @@ require (
 
 require (
 	firebase.google.com/go v3.13.0+incompatible
-	github.com/aws/aws-sdk-go v1.55.6
+	github.com/aws/aws-sdk-go-v2/config v1.29.9
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.62
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.17.66
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.78.2
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
--- pkg/aws/setup.go
+++ pkg/aws/setup.go
@@ -2,44 +2,36 @@ package aws
 
 import (
 	"bytes"
+	"context"
 	"encoding/base64"
 	"fmt"
 	"mime/multipart"
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 type S3 struct {
-	Client     *s3.S3
+	Client     *s3.Client
 	BucketName string
 }
 
-func Connect(ak, r, as, b string) (s *S3, err error) {
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String(r),
-		Credentials: credentials.NewStaticCredentials(ak, as, ""),
-	})
+func Connect(bucket string, cfg aws.Config, optFns ...func(*s3.Options)) (s *S3, err error) {
 
-	if err != nil {
-		return
-	}
-
-	svc := s3.New(sess)
+	s3Client := s3.NewFromConfig(cfg, optFns...)
 
 	s = &S3{
-		Client:     svc,
-		BucketName: b,
+		Client:     s3Client,
+		BucketName: bucket,
 	}
 
 	return
 }
 
-func (s S3) UploadFileToS3(fileName, base64File, contentType string) (string, error) {
+func (s *S3) UploadFileToS3(ctx context.Context, fileName, base64File, contentType string) (string, error) {
 
 	b64data := base64File[strings.IndexByte(base64File, ',')+1:]
 
@@ -49,27 +41,24 @@ func (s S3) UploadFileToS3(fileName, base64File, contentType string) (string, er
 		fmt.Println("Error uploading file:", err)
 		return "", err
 	}
-	_, err = s.Client.PutObject(&s3.PutObjectInput{
+	_, err = s.Client.PutObject(ctx, &s3.PutObjectInput{
 		Bucket:      aws.String(s.BucketName),
 		Key:         aws.String(fileName),
 		Body:        bytes.NewReader(decode),
 		ContentType: aws.String(contentType),
 	})
-
 	if err != nil {
-		fmt.Println("Error uploading file:", err)
-		return "", err
+		return "", fmt.Errorf("error uploading file to S3: %w", err)
 	}
+
 	return fileName, nil
 }
 
-func (s S3) UploadApptFileToS3(fileType string, file multipart.File, patientId string, extn string, contentType string) (string, error) {
-	date := time.Now().Format(time.RFC3339)
-	key := fileType + "/" + patientId + "/" + date + extn
-
-	_, err := s.Client.PutObject(&s3.PutObjectInput{
+func (s *S3) UploadApptFileToS3(ctx context.Context, filename string, file multipart.File, contentType string) (string, error) {
+	uploader := manager.NewUploader(s.Client)
+	_, err := uploader.Upload(ctx, &s3.PutObjectInput{
 		Bucket:      aws.String(s.BucketName),
-		Key:         aws.String(key),
+		Key:         aws.String(filename),
 		Body:        file,
 		ContentType: aws.String(contentType),
 	})
@@ -78,39 +67,28 @@ func (s S3) UploadApptFileToS3(fileType string, file multipart.File, patientId s
 		fmt.Println("Error uploading file:", err)
 		return "", err
 	}
-	return key, nil
+	return filename, nil
 }
 
-func (s S3) DeleteFileFromS3(key string) error {
-
-	input := &s3.DeleteObjectInput{
+func (s *S3) DeleteFileFromS3(ctx context.Context, key string) error {
+	_, err := s.Client.DeleteObject(ctx, &s3.DeleteObjectInput{
 		Bucket: aws.String(s.BucketName),
 		Key:    aws.String(key),
-	}
-
-	_, err := s.Client.DeleteObject(input)
-
-	return err
-}
-
-func (s S3) GetFileLink(key string) (string, error) {
-
-	params := &s3.GetObjectInput{
-		Bucket: aws.String(s.BucketName),
-		Key:    aws.String(key),
-	}
-
-	req, _ := s.Client.GetObjectRequest(params)
-
-	if req.Error != nil {
-		fmt.Println(req.Error)
-		return "", req.Error
-	}
-
-	url, err := req.Presign(7 * 24 * time.Hour)
+	})
 	if err != nil {
-		return "", err
+		return fmt.Errorf("error deleting file from S3: %w", err)
 	}
+	return nil
+}
 
-	return url, err
+func (s *S3) GetFileLink(ctx context.Context, key string) (string, error) {
+	presignClient := s3.NewPresignClient(s.Client)
+	presignedURL, err := presignClient.PresignGetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(s.BucketName),
+		Key:    aws.String(key),
+	}, s3.WithPresignExpires(7*24*time.Hour))
+	if err != nil {
+		return "", fmt.Errorf("error generating presigned URL: %w", err)
+	}
+	return presignedURL.URL, nil
 }
